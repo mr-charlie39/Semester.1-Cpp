448e63ff1fdebbdc2f4ec62bc0a3e59db41a489a
Muhammad Ali Abid <mali1122330309@gmail.com>
2025-11-26 20:46:20 +0500

    New File

--- 2D - Array-8.cpp
+++ 2D - Array-8.cpp
@@ -2,34 +2,37 @@
 using namespace std;
 int main(){
 	
-	int arr1[3][3];
-	int arr2[3][3];
-	int matrix[3][3];
+	int arr1[2][2];
+	int arr2[2][2];
+	int matrix[2][2];
 	
 	cout<<"<----- Array:1 ----->"<<endl;
-	for(int i = 0 ; i < 3 ; i++){
-		for(int j = 0 ; j < 3 ; j++){
+	for(int i = 0 ; i < 2 ; i++){
+		for(int j = 0 ; j < 2 ; j++){
 			cin>>arr1[i][j];
 		}
 	}
 	
 	cout<<"<----- Array:2 ----->"<<endl;
-	for(int i = 0 ; i < 3 ; i++){
-		for(int j = 0 ; j < 3 ; j++){
+	for(int i = 0 ; i < 2 ; i++){
+		for(int j = 0 ; j < 2 ; j++){
 			cin>>arr2[i][j];
 		}
 	}
 	
-	for(int i = 0 ; i < 3 ; i++){
-		for(int j = 0 ; j < 3 ; j++){
-			matrix[i][j] = arr1[i][j]*arr2[i][j];
+	for(int i = 0 ; i < 2 ; i++){
+		for(int j = 0 ; j < 2 ; j++){
+			matrix[i][j] = 0;
+			for(int s = 0 ; s < 2 ; s++){
+				matrix[i][j] += (arr1[i][s]*arr2[s][j]);
+			}
 		}
 	}
 	
 	cout<<"\t<----- Matrix: ----->"<<endl;
-	for(int i = 0 ; i < 3 ; i++){
-		for(int j = 0 ; j < 3 ; j++){
-			cout<<"\t"<<arr1[i][j]<<" * "<<arr2[i][j]<<" = "<<matrix[i][j];
+	for(int i = 0 ; i < 2 ; i++){
+		for(int j = 0 ; j < 2 ; j++){
+			cout<<"\t"<<matrix[i][j];
 		}
 		cout<<endl;
 	}
